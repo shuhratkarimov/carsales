06cb6abd03b0aa632b90bc6e0d53ab2b7b3a50cf
shuhrat <n395b421na@gmail.com>
2025-02-02 11:17:13 +0500

    added

--- Controller/file.upload.controller.js
+++ Controller/file.upload.controller.js
@@ -1,13 +1,20 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 const CarsModel = require("../Schemas/cars.schema");
-const BaseError = require("../Utils/base_error")
+const BaseError = require("../Utils/base_error");
 
- async function fileUploader(req, res, next) {
+
+const uploadDir = path.join(__dirname, "../uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+async function fileUploader(req, res, next) {
   try {
     const storage = multer.diskStorage({
       destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, "../uploads"));
+        cb(null, uploadDir);
       },
       filename: (req, file, cb) => {
         const fileExtension = path.extname(file.originalname);
@@ -15,26 +22,28 @@ const BaseError = require("../Utils/base_error")
         cb(null, fileNewName);
       },
     });
+
     const upload = multer({ storage: storage });
+
     upload.array("pictures", 3)(req, res, async (err) => {
       if (err) {
-        return next(err)
+        return next(err);
       }
       if (req.files.length !== 3) {
-        return next(BaseError.BadRequest(404, "Yuklanadigan rasmlar soni 3 ta bo'lishi kerak!"))
+        return next(BaseError.BadRequest(404, "Yuklanadigan rasmlar soni 3 ta bo'lishi kerak!"));
       }
+
       const fileLinks = req.files.map(
         (file) => `${req.protocol}://${req.get("host")}/uploads/${file.filename}`
       );
+
       const id = req.params.carId;
-      const car = await CarsModel.findByIdAndUpdate(
-        id,
-        { img: fileLinks },
-        { new: true }
-      );
+      const car = await CarsModel.findByIdAndUpdate(id, { img: fileLinks }, { new: true });
+
       if (!car) {
         return res.status(404).json({ message: "Avtomobil topilmadi!" });
       }
+
       res.status(201).json({
         message: "Rasmlar muvaffaqiyatli yuklandi",
         images: fileLinks,
@@ -43,8 +52,6 @@ const BaseError = require("../Utils/base_error")
   } catch (error) {
     next(error);
   }
-};
+}
 
-module.exports = fileUploader
-
-
+module.exports = fileUploader;
